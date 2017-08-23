23e83f157b948c0873321d1d619b3b524362712d
18815593959 <1013104194@qq.com>
2017-08-23 18:36:27 +0800

    test

--- install.sh
+++
@@ -1,7 +0,0 @@
-cd /root
-yum install git
-git clone https://github.com/xuwei95/shadowsocksr
-
-echo "alias ssr='python /root/main.py'" >> /etc/bashrc
-source /etc/.bashrc
-echo "install success,input 'ssr'to config ..."
